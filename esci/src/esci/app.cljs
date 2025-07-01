f95bbb4dfdf6fe561a33b19f6faf2e6e00175ef7
qijun <qijun.wei@hey.com>
2025-07-01 19:50:09 +0800

    feat: build esci

--- esci/src/esci/app.cljs
+++ esci/src/esci/app.cljs
@@ -1,11 +1,18 @@
-(ns esci.app)
+(ns esci.app
+  (:require [sci.core :as sci]))
 
 ;; Entry point for the application
 (defn ^:export init []
   (println "Hello from ClojureScript!")
   (js/console.log "Application initialized"))
 
-;; Initialize the app when the page loads
-(defn ^:export main []
-  (init))
-
+;; Execute function that evaluates Clojure code with context
+(defn ^:export execute [code context]
+  (try
+    (let [ctx-map (js->clj context :keywordize-keys true)
+          options {:namespaces {'user ctx-map}}
+          result (sci/eval-string code options)]
+      result)
+    (catch js/Error e
+      (js/console.error "Execution error:" e)
+      nil)))
